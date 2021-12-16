1216e09d9d8faf51ffe774e0547e5d5a078d11f0
Daniel23112002 <noize337@gmail.com>
2021-12-16 20:09:06 +0700

    first commit

---
+++ resources/views/post.blade.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>post</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+  <body>
+    <section class="projects-section bg-light" id="posts">
+        <div class="row gx-0 mb-5 mb-lg-0 justify-content-center">
+            <div class="col-lg-6"><img class="img-fluid" src="https://cdn.britannica.com/q:60/67/198167-131-E01A159F/German-soldiers-Battle-of-Stalingrad-January-1943.jpg" alt="..." /></div>
+            <div class="col-lg-6">
+                <div class="bg-black text-center h-100 project">
+                    <div class="d-flex h-100">
+                        <div class="project-text w-100 my-auto text-center text-lg-left">
+                            <h4 class="text-white">Battle of Stalingrad 23 August 1942</h4>
+                            <p class="mb-0 text-white-50">Battle of Stalingrad, (July 17, 1942–February 2, 1943), successful Soviet defense of the city of Stalingrad (now Volgograd), Russia, U.S.S.R. , during World War II. ... It stopped the German advance into the Soviet Union and marked the turning of the tide of war in favour of the Allies.</p>
+                            <br>
+                            <a class="btn btn-primary" href="stalin">More Details</a>
+                            <hr class="d-none d-lg-block mb-0 ms-0" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Project Two Row-->
+        <div class="row gx-0 justify-content-center">
+            <div class="col-lg-6"><img class="img-fluid" src="https://cdni.rbth.com/rbthmedia/images/2020.05/original/5eaf99f315e9f90a8762ef23.jpg" alt="..." /></div>
+            <div class="col-lg-6 order-lg-first">
+                <div class="bg-black text-center h-100 project">
+                    <div class="d-flex h-100">
+                        <div class="project-text w-100 my-auto text-center text-lg-right">
+                            <h4 class="text-white">Battle of Berlin 16 April 1945</h4>
+                            <p class="mb-0 text-white-50">The Battle of Berlin resulted in the surrender of the German army and the death of Adolf Hitler (by suicide). It was a resounding victory for the Soviet Union and the Allies. The battle took its toll on both sides, however. Around 81,000 Soviet Union soldiers were killed and another 280,000 were wounded.</p>
+                            <br>
+                            <a class="btn btn-primary" href="berlin">More Details</a>
+                            <hr class="d-none d-lg-block mb-0 me-0" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+  </body>
+</html>
